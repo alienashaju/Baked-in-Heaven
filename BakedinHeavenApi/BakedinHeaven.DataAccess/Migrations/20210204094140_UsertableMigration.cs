069f4bade681224e12a00e7a1780d511616c990b
aliena.shaju <aliena.shaju@ALIENASHAJU>
2021-02-05 19:31:21 +0530

    committed

---
+++ BakedinHeavenApi/BakedinHeaven.DataAccess/Migrations/20210204094140_UsertableMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BakedinHeaven.DataAccess.Migrations
+{
+    public partial class UsertableMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phonenumber",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phonenumber",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
